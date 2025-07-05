7405c8d88bc190da0cec21f1a01c5cd755a1863d
abdul hamid <abdulachik@icloud.com>
2025-07-04 22:08:58 -0600

    fix: add missing exports for build
    - Export MEXICO_STATES array from shipping-zones.ts
    - Add formatCurrency function to utils.ts
    - Fix import errors in vendor shipping settings and checkout pages

--- lib/utils/shipping-zones.ts
+++ lib/utils/shipping-zones.ts
@@ -1,3 +1,39 @@
+// All Mexican states
+export const MEXICO_STATES = [
+  "Aguascalientes",
+  "Baja California",
+  "Baja California Sur",
+  "Campeche",
+  "Chiapas",
+  "Chihuahua",
+  "Ciudad de México",
+  "Coahuila",
+  "Colima",
+  "Durango",
+  "Estado de México",
+  "Guanajuato",
+  "Guerrero",
+  "Hidalgo",
+  "Jalisco",
+  "Michoacán",
+  "Morelos",
+  "Nayarit",
+  "Nuevo León",
+  "Oaxaca",
+  "Puebla",
+  "Querétaro",
+  "Quintana Roo",
+  "San Luis Potosí",
+  "Sinaloa",
+  "Sonora",
+  "Tabasco",
+  "Tamaulipas",
+  "Tlaxcala",
+  "Veracruz",
+  "Yucatán",
+  "Zacatecas"
+];
+
 // Mexican states grouped by shipping zones
 export const MEXICO_SHIPPING_ZONES = {
   central: {
