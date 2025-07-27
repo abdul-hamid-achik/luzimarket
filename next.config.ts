de2c2b912550a45856ab8b8c386c04952b9ca0c8
abdul hamid <abdulachik@icloud.com>
2025-07-27 00:30:17 -0600

    refactor: migrate next.config from JavaScript to TypeScript

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,18 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Suppress Edge Runtime warnings for Node.js modules
+      config.ignoreWarnings = [
+        { module: /node_modules\/bcryptjs/ },
+        { module: /node_modules\/postgres/ },
+        { message: /A Node\.js module is loaded/ },
+        { message: /A Node\.js API is used/ },
+      ];
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
@@ -23,7 +35,36 @@ const nextConfig: NextConfig = {
         hostname: '*.public.blob.vercel-storage.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+        ],
+      },
+    ];
   },
   experimental: {
     serverActions: {
